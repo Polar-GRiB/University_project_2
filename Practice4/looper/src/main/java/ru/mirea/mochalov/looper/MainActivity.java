55fc7f8abea22050da30543fad95fd85ccc94720
Polar-GRiB <56265901+Polar-GRiB@users.noreply.github.com>
2021-06-07 23:53:05 +0300

    Update MainActivity.java

--- Practice4/looper/src/main/java/ru/mirea/mochalov/looper/MainActivity.java
+++ Practice4/looper/src/main/java/ru/mirea/mochalov/looper/MainActivity.java
@@ -1,20 +1,17 @@
 package ru.mirea.mochalov.looper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Message;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     MyLooper myLooper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         myLooper = new MyLooper();
         myLooper.start();
     }
